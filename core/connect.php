abc4e94127affcfa394c29f8501ec7527e084236
Yousef <117045051+vizkex@users.noreply.github.com>
2024-01-17 17:02:47 +0300

    Update connect.php

--- core/connect.php
+++ core/connect.php
@@ -2,4 +2,4 @@
 
 
 
-$conn = mysqli_connect('db-mysql-fra1-28431-do-user-15042680-0.c.db.ondigitalocean.com','doadmin','AVNS_bryK0iIxWUcLhXuAH5y ','defaultdb' ,'25060') or die('connect failed');
+$conn = mysqli_connect('private-db-mysql-fra1-28431-do-user-15042680-0.c.db.ondigitalocean.com','doadmin','AVNS_bryK0iIxWUcLhXuAH5y','defaultdb') or die('connect failed');
